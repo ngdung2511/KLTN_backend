ca70cbcc615389223d45110ee31b5f14c38181b9
Phí Minh Hiếu <98246591+Angelectronic@users.noreply.github.com>
2025-03-21 22:13:31 +0700

    feat: integrate image processing for answer sheet analysis and update schema

--- mvc/controller/answer_sheet.py
+++ mvc/controller/answer_sheet.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter, status, UploadFile,File
 from ..model.upload import upload_photo
 from ..model.answer_sheet import AnswerSheetSchema
-from typing import List, Union
+from typing import List
 from ..model import answer_sheet
+import base64
+import anthropic
+import ast
+
 
 router = APIRouter(prefix="/answer_sheet", tags=["Answer Sheet"])
+client = anthropic.Anthropic()
+prompt = open("mvc/controller/prompt.txt", "r", encoding="utf-8").read()
+
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
 async def upload(files: List[UploadFile] = File(...)):
@@ -13,8 +20,53 @@ async def upload(files: List[UploadFile] = File(...)):
         file_name = file.filename
         studentName = file_name.split(".")[0].split("_")[1]
         studentCode = file_name.split(".")[0].split("_")[0]
+        
+        # change image to base64
+        content = await file.read()
+        encoded_string = base64.b64encode(content).decode("utf-8")        
+
+        # use LLM to get answer
+        message = client.messages.create(
+            model="claude-3-7-sonnet-20250219",
+            max_tokens=1024,
+            system=[
+                {
+                    "type": "text",
+                    "text": prompt,
+                    "cache_control": {"type": "ephemeral"}
+                }
+            ],
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                            "source": {
+                                "type": "base64",
+                                "media_type": "image/jpeg",
+                                "data": encoded_string
+                            }
+                        },
+                        {
+                            "type": "text",
+                            "text": "Identify which options have been filled."
+                        }
+                    ]
+                }
+            ]
+        )
+        content = message.content[0].text
+        list_answer = []
+        try:
+            list_answer = ast.literal_eval(content)
+        except:
+            raise ValueError("Error in processing the image. Please try again.")
+        
         imageId = upload_photo(file)
-        answer_sheet_data = AnswerSheetSchema(imageId=imageId['file_id'], studentName=studentName, studentCode=studentCode)
+
+        
+        answer_sheet_data = AnswerSheetSchema(imageId=imageId['file_id'], studentName=studentName, studentCode=studentCode, detectedAnswers=list_answer)
         answer_sheet.insert_answer_sheet(answer_sheet_data)
         results.append(imageId)
     return {"message": "Answer sheets uploaded successfully", "results": results}
@@ -23,5 +75,3 @@ async def upload(files: List[UploadFile] = File(...)):
 @router.post("/list", status_code=status.HTTP_201_CREATED)
 async def list_answer_sheets():
     return answer_sheet.get_all_answer_sheets()
-
-
